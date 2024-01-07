6ead84c81f10cc4cea8452aaa382154c356b3d3c
Adna <adnaoliveirasilva09@gmail.com>
2024-01-07 10:40:59 -0300

    extremamente básico

---
+++ uri1001.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int a, b;
+    printf("digite o valor de a: ");
+    scanf("%d", &a);
+    printf("digite o valor de b: ");
+    scanf("%d", &b);
+    int x = a + b;
+    printf("x = %d\n", x);
+
+    return 0;
+}
