729ec802df7425581dbec259798ddfdc8788c1ac
Adna <adnaoliveirasilva09@gmail.com>
2024-01-07 11:14:05 -0300

    produto simples

---
+++ uri1004.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int a, b;
+    printf("digite o valor de a:\n");
+    scanf("%d", &a);
+    printf("digite o valor de b:\n");
+    scanf("%d", &b);
+    int PROD = a*b;
+    printf("PROD= %d", PROD);
+
+    return 0;
+}
