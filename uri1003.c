33cfc9ea010c3993a662fd920009cd38730f40c7
Adna <adnaoliveirasilva09@gmail.com>
2024-01-07 11:06:40 -0300

    soma simples

---
+++ uri1003.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int A, B;
+    printf("digite o valor de A:\n");
+    scanf("%d", &A);
+    printf("digite o valor de B:\n");
+    scanf("%d", &B);
+    int SOMA = A+B;
+    printf("SOMA = %d\n", SOMA);
+
+    return 0;
+}
